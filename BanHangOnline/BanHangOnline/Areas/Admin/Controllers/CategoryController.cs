f537e2d11d033425ced34cf2fd19c4f38af03f19
Ancol-Developer <trandieu026@gmail.com>
2024-12-22 13:55:48 +0700

    Add view Add Category

---
+++ BanHangOnline/BanHangOnline/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BanHangOnline.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly WebStoreDbContext _db;
+        public CategoryController(WebStoreDbContext dbContext)
+        {
+            _db= dbContext;
+        }
+
+        // GET: Admin/Category
+        public IActionResult Index()
+        {
+            var items = _db.Category.ToList();
+            return View(items);
+        }
+
+        // GET: Admin/Category/Add
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // Post: Admin/Category/Add
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(Category category)
+        {
+            return View();
+        }
+    }
+}
