7ce0215064aa7fe267c9d51bcf78ea680356a0aa
Ancol-Developer <trandieu026@gmail.com>
2024-12-22 23:26:17 +0700

    "Add" view

--- BanHangOnline/BanHangOnline/Areas/Admin/Controllers/CategoryController.cs
+++ BanHangOnline/BanHangOnline/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Entities;
+using Entities.Common;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BanHangOnline.Areas.Admin.Controllers
@@ -9,7 +10,7 @@ namespace BanHangOnline.Areas.Admin.Controllers
         private readonly WebStoreDbContext _db;
         public CategoryController(WebStoreDbContext dbContext)
         {
-            _db= dbContext;
+            _db = dbContext;
         }
 
         // GET: Admin/Category
@@ -28,9 +29,18 @@ namespace BanHangOnline.Areas.Admin.Controllers
         // Post: Admin/Category/Add
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Add(Category category)
+        public async Task<IActionResult> Add(Category model)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                model.CreateDate = DateTime.Now;
+                model.ModifierDate = DateTime.Now;
+                model.Alias = Filter.FilterChar(model.Title ?? string.Empty);
+                await _db.Category.AddAsync(model);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
     }
 }
--- BanHangOnline/Entities/Category.cs
+++ BanHangOnline/Entities/Category.cs
@@ -14,10 +14,16 @@ public class Category : CommonAbstract
     [Key]
     [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    [Required(ErrorMessage ="Tên danh mục không được để trống")]
+    [StringLength(150)]
     public string? Title { get; set; }
+    public string? Alias { get; set; }
     public string? Description { get; set; }
+    [StringLength(150)]
     public string? SeoTitle { get; set; }
+    [StringLength(250)]
     public string? SeoDescription { get; set;}
+    [StringLength(150)]
     public string? SeoKeywords { get; set; }
     public int Position { get; set; }
 
