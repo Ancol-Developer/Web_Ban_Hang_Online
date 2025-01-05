23c95d59b4dd453361da69243818b9d77c05fdd6
Ancol-Developer <trandieu026@gmail.com>
2025-01-05 22:58:28 +0700

    Product Controller: - View Index - VIew Add (sample)

---
+++ BanHangOnline/BanHangOnline/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using BanHangOnline.Common;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BanHangOnline.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly WebStoreDbContext _db;
+        public ProductsController(WebStoreDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public IActionResult Index(string searchString, int? pageNumber)
+        {
+            int pageSize = 10;
+            var items = _db.Product.OrderByDescending(x => x.Id).ToList();
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                items = items.Where(s => s.Alias?.Contains(searchString) == true || s.Title?.Contains(searchString) == true).ToList();
+            }
+
+            ViewBag.PageSize = pageSize;
+
+            // Save Search string
+            ViewBag.SearchText = searchString;
+            return View(PaginatedList<Product>.CreateAsync(items, pageNumber ?? 1, pageSize));
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
