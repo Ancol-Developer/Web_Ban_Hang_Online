04f727f4d033a0fd1b84f174195c0e41bbbc6f9a
Ancol-Developer <trandieu026@gmail.com>
2025-02-09 13:23:36 +0700

    Draff

---
+++ BanHangOnline/BanHangOnline/Controllers/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BanHangOnline.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly WebStoreDbContext _db;
+
+        public MenuController(WebStoreDbContext db)
+        {
+            this._db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MenuTop()
+        {
+            var items = _db.Category.OrderBy(x => x.Position).ToList();
+            return PartialView("_MenuTop", items);
+        }
+    }
+}
