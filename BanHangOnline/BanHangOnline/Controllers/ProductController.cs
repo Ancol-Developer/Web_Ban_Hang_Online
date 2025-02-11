fa4e47f4e4aa4efb83f125775f4adba70fd7a50e
Ancol_Developer <trandieu026@gmail.com>
2025-02-11 23:13:34 +0700

    Add view component

---
+++ BanHangOnline/BanHangOnline/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BanHangOnline.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly WebStoreDbContext _db;
+
+		public ProductController(WebStoreDbContext db)
+        {
+			this._db = db;
+		}
+
+        public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ BanHangOnline/BanHangOnline/ViewComponents/MenuProductCategoryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BanHangOnline.ViewComponents
+{
+	public class MenuProductCategoryViewComponent : ViewComponent
+	{
+		private readonly WebStoreDbContext _db;
+		public MenuProductCategoryViewComponent(WebStoreDbContext db)
+		{
+			this._db = db;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var items = await _db.ProductCategory.ToListAsync();
+			return View(items);
+		}
+	}
+}
