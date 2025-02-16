db074c12ea8f6f6d4d4d63df0433f300aae03f0a
Ancol-Developer <trandieu026@gmail.com>
2025-02-16 12:57:45 +0700

    draff

---
+++ BanHangOnline/BanHangOnline/ViewComponents/MenuLeftViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BanHangOnline.ViewComponents
+{
+    public class MenuLeftViewComponent : ViewComponent
+    {
+        private readonly WebStoreDbContext _db;
+        public MenuLeftViewComponent(WebStoreDbContext db)
+        {
+            this._db = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _db.ProductCategory.ToListAsync();
+            return View(items);
+        }
+    }
+}
