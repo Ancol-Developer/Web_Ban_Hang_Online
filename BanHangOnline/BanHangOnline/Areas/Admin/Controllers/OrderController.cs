b8494721ef79f0515fd72e0b017d81d880bf49fa
Ancol_Developer <trandieu026@gmail.com>
2025-03-14 21:22:07 +0700

    draff

---
+++ BanHangOnline/BanHangOnline/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using BanHangOnline.Common;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Printing;
+
+namespace BanHangOnline.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly WebStoreDbContext _db;
+        public OrderController(WebStoreDbContext db)
+        {
+			_db = db;
+		}
+        public IActionResult Index(int? page)
+		{
+			var items = _db.Order.OrderByDescending(x => x.CreateDate).ToList();
+			if (page is null)
+			{
+				page = 1;
+			}
+
+			int pageSize = 10;
+			return View(Common<Order>.CreateAsync(items, page ?? 1, pageSize));
+		}
+	}
+}
